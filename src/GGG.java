2b105cd774c252de13c677062b352feb8050a66b
yulian <batkovna1987@mail.ru>
2021-07-14 21:35:58 +0300

    3 дз

---
+++ src/GGG.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class GGG {
+      public static void main (String[] args){
+        nomer6Arr( 5,111);
+
+          int[] nomer8={12,13,14,15,3,4,7,2,8,9};
+          Arrays.sort(nomer8);
+          for (int j=0;j<nomer8.length; j++ ){
+              if (nomer8(j)<nomer8(j)+1)
+                  nomer8(j)=min;
+              }
+                      }
+              System.out.println(Arrays.toString(nomer8));
+    }
+        public static void nomer6Arr(int len, int initialValue){
+                     int [] nomer6= new int[len];
+            for(int n=0; n<nomer6.length;n++) {
+                nomer6[n] = initialValue;
+            }
+            System.out.println(Arrays.toString(nomer6));
+
+    }
+
+}
---
+++ src/TreeLesson.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.util.Arrays;
+
+public class TreeLesson {
+    public static void main(String[] args) {
+        nomer5Arr(5,111);
+
+        int[] nomer1 = {1, 0, 1, 1, 0, 1};
+        for (int i = 0; i < nomer1.length; i++)
+            if (nomer1[i] == 0)
+                nomer1[i] = 1;
+            else
+                nomer1[i] = 0;
+        System.out.println(Arrays.toString(nomer1));
+
+        int[] nomer2 = new int[5];
+        for (int j = 0; j < nomer2.length; j++) {
+            nomer2[j] = j + 1;
+        }
+            System.out.println(Arrays.toString(nomer2));
+
+        int[] nomer3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int k = 0; k < nomer3.length; k++) {
+            if (nomer3[k] <6)
+                nomer3[k] = (nomer3[k]* 2);
+        }
+                    System.out.println(Arrays.toString(nomer3));
+
+        int [][] nomer4 = new int[4][5];
+        for (int l = 0; l < nomer4.length; l++) {
+            for (int m = 0; m < nomer4[l].length; m++){
+                if (l==m)
+                    nomer4[l][m]=2;
+                    else
+                        nomer4[l][m]=1;
+            }
+        }
+            for (int l=0;l<nomer4.length;l++) {
+                String nomer4Arr=Arrays.toString(nomer4[l]);
+                System.out.println(nomer4Arr);
+            }
+  }
+  public static void nomer5Arr (int len, int initialValue){
+                int [] nomer5= new int[len];
+            for(int n=0; n<nomer5.length;n++) {
+            nomer5[n] = initialValue;
+        }
+        System.out.println(Arrays.toString(nomer5));
+
+    }
+    }
